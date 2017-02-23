1a4ccdede60294c699f36f51ce1f5c562c8cc0c3
rohitcs079 <rohitcs079@gmail.com>
2017-02-24 00:27:57 +0530

    First commit

---
+++ src/main/java/com/usecase/MeterDataApplication.java
@@ -0,0 +1,12 @@
+package com.usecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeterDataApplication.class, args);
+	}
+}
